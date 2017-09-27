915eedcda747cebfa44c5afdece90829bfb7b672
Chanachai Puttaruksa <Chanachai_mig@hotmail.com>
2017-09-27 23:43:57 +0700

    complete region picker opencv

--- 27_region_picker_opencv.py
+++ 27_region_picker_opencv.py
@@ -12,7 +12,7 @@ total_click = 3
 # mouse callback function
 def draw_rectangle1(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.rectangle(img1, (x - 2, y - 2), (x + 2, y + 2), (0, 255, 0), 1)
+        cv2.rectangle(img1, (x, y), (x + 5, y + 5), (0, 255, 0), 1)
         img1_cord.append([x, y])
         print(img1[x, y])
         print(x, y)
@@ -21,7 +21,7 @@ def draw_rectangle1(event,x,y,flags,param):
 # mouse callback function
 def draw_rectangle2(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.rectangle(img2, (x - 2, y - 2), (x + 2, y + 2), (0, 255, 0), 1)
+        cv2.rectangle(img2, (x, y), (x + 5, y + 5), (0, 255, 0), 1)
         img2_cord.append([x, y])
         print(img2[x, y])
         print(x, y)
@@ -59,12 +59,19 @@ def select_image():
 
 
 def save_data():
+    global path
+
+    the_img1 = cv2.imread(path + '/img1.jpg')
+    the_img2 = cv2.imread(path + '/img2.jpg')
+
     for num in range(total_click):
         print(num)
         x1 = img1_cord[num][0]
         y1 = img1_cord[num][1]
-        print("image 1 pixel : {}".format(img1[x1, y1]))
-        tiny_crop_one = img1[x1 - 2: y1 - 2, x1 + 2:y1 + 2]
+        print("image 1 pixel : {}".format(the_img1[x1, y1]))
+        tiny_crop_one = the_img1[y1:y1 + 5, x1:x1 + 5]
+        cv2.imwrite(path + '/img1_tiny_crop_' + str(num) + '.jpg', tiny_crop_one)
+        print(tiny_crop_one.shape)
         data_one = tiny_crop_one.ravel()
         data_one = data_one.astype(np.int64)
 
@@ -72,31 +79,41 @@ def save_data():
         for i in range(data_one.shape[0]):
             data_set_one.append(str(data_one[i]) + " ")
 
-        # ---------------------------------------------------
-
         x2 = img2_cord[num][0]
         y2 = img2_cord[num][1]
-        print("image 2 pixel : {}".format(img2[x2, y2]))
-        tiny_crop_two = img2[x2 - 2: y2 - 2, x2 + 2:y2 + 2]
+        print("image 2 pixel : {}".format(the_img2[x2, y2]))
+        tiny_crop_two = the_img2[y2:y2 + 5, x2:x2 + 5]
+        cv2.imwrite(path + '/img2_tiny_crop_' + str(num) + '.jpg', tiny_crop_two)
+        print(tiny_crop_two.shape)
         data_two = tiny_crop_two.ravel()
         data_two = data_two.astype(np.int64)
 
-        data_set_two = [" |features "]
+        data_set_two = ["|features "]
         for i in range(data_two.shape[0]):
             data_set_two.append(str(data_two[i]) + " ")
         data_set_two.append('\n')
+
         if num == 0:
-            with open(path + 'test-data.txt', 'w') as outfile:
+            with open(path + '/train-data.txt', 'w') as outfile:
                 for data_line in data_set_one:
                     outfile.write(data_line)
                 for data_line in data_set_two:
                     outfile.write(data_line)
         else:
-            with open(path + 'test-data.txt', 'a') as outfile:
+            with open(path + '/train-data.txt', 'a') as outfile:
                 for data_line in data_set_one:
                     outfile.write(data_line)
                 for data_line in data_set_two:
                     outfile.write(data_line)
+
+        print(data_set_one)
+        print(len(data_set_one))
+
+        print(data_set_two)
+        print(len(data_set_two))
+
+    cv2.imwrite(path + '/img1_edit.jpg', img1)
+    cv2.imwrite(path + '/img2_edit.jpg', img2)
 
 
 root = Tk()
--- tester.py
+++ tester.py
@@ -10,8 +10,13 @@ print(img[1125, 2233])
 
 
 path = 'C:/Users/Mig/Desktop/test_image/img2.jpg'
+path2 = 'C:/Users/Mig/Desktop/emerium_1.jpg'
 
 img = cv2.imread(path)
-print(img[1359, 2386])
-print(img[1578, 2854])
-print(img[1741, 2783])
+
+tiny_crop = img[3096:3096 + 100, 1246:1246 + 100]
+print(tiny_crop[0][0])
+print(tiny_crop.shape)
+cv2.imshow('tiny-crop', tiny_crop)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
